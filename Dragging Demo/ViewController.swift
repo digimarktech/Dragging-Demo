9e39a47c0c063ea2ca3d343b8aa50d9503303d74
Marc Aupont <marc@digimarktech.com>
2017-02-16 23:06:00 -0500

    Completed Dragging Demo App ios9.3

--- Dragging Demo/ViewController.swift
+++ Dragging Demo/ViewController.swift
@@ -13,6 +13,65 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let label = UILabel(frame: CGRectMake(self.view.bounds.width / 2 - 100, self.view.bounds.height / 2 - 50, 200, 100 ))
+        
+        label.text = "Drag Me!"
+        
+        label.textAlignment = NSTextAlignment.Center
+        
+        label.textColor = UIColor.redColor()
+        
+        self.view.addSubview(label)
+        
+        let gesture = UIPanGestureRecognizer(target: self, action: Selector("wasDragged:"))
+        
+        label.addGestureRecognizer(gesture)
+        
+        label.userInteractionEnabled = true
+    }
+    
+    func wasDragged(gesture:UIPanGestureRecognizer) {
+        
+        let translation = gesture.translationInView(self.view)
+        
+        let label = gesture.view!
+        
+        label.center = CGPoint(x: self.view.bounds.width / 2 + translation.x, y: self.view.bounds.height / 2 + translation.y)
+        
+        //This line measures how far the label has been moved from the center. Value will be positive if we drag to the right and negative if we drag to the left
+        let xFromCenter = label.center.x - self.view.bounds.width / 2
+        
+        let scale = min(100 / abs(xFromCenter), 1)
+        
+        var rotation = CGAffineTransformMakeRotation(xFromCenter / 200)
+        
+        var stretch = CGAffineTransformScale(rotation, scale, scale)
+        
+        label.transform = stretch
+        
+
+        
+        if gesture.state == UIGestureRecognizerState.Ended {
+            
+            if label.center.x < 100 {
+                
+                print("Not chosen")
+                
+            }else if label.center.x > self.view.bounds.width - 100 {
+                
+                print("chosen")
+            }
+            
+            rotation = CGAffineTransformMakeRotation(0)
+            
+            stretch = CGAffineTransformScale(rotation, 1, 1)
+            
+            label.transform = stretch
+            
+            label.center = CGPoint(x: self.view.bounds.width / 2, y: self.view.bounds.height / 2)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
